09a416f200b4b4ff9e36e0ef82e7db762cd8677b
"francestu96" <"francestu96@gmail.com">
2022-04-28 10:42:37 +0200

    getNextDepositTime() added

--- ApeSkulls.sol
+++ ApeSkulls.sol
@@ -15,13 +15,25 @@ interface IBEP20 {
 }
 
 contract ApeSkulls {
+    // roastedbeef.io: 10% BnB
+    // 86 400 000 000
+    //        864 000
+    
+    // rocket-game.io: 10% busdt
+    //  86 400 000 000
+    //         864 000
+
+    // BNB Miner: 3% BnB
+    // 259 200 000 000
+    //       2 592 000
+    
     // marketSkulls              = 108 000 000 000
     // SKULLS_TO_COLLECT_1MINERS =       1 080 000  = marketSkulls / 10^5
     // PSN                       =          10 000  = SKULLS_TO_COLLECT_1MINERS / 10^2+8
     // PSNH                      =           5 000  = PSN / 2
 
-    uint256 private SKULLS_MARKET_INIT = 108000000000;
-    uint256 private SKULLS_TO_COLLECT_1MINERS = 1080000;
+    uint256 private SKULLS_MARKET_INIT = 259200000000;
+    uint256 private SKULLS_TO_COLLECT_1MINERS = 2592000;
     uint256 private PSN = 10000;
     uint256 private PSNH = 5000;
     uint256 private DEV_FEES = 5;
@@ -54,17 +66,17 @@ contract ApeSkulls {
     }
 
     function buySkulls(uint256 amount, address ref) public {
-        require(initialized);
+        require(initialized, "Contract has not been initialized yet! Wait the admin to start it");
         uint256 skullsBought = calculateSkullBuy(amount, blackApe.balanceOf(address(this)) - amount);
         uint256 fees = _devFee(amount);
         skullsBought = skullsBought - fees;
         blackApe.transfer(owner, fees);
         claimedSkulls[msg.sender] = claimedSkulls[msg.sender] + skullsBought;
-        hatchSkulls(ref);
+        burySkulls(ref);
     }
     
-    function hatchSkulls(address ref) public {
-        require(initialized);
+    function burySkulls(address ref) public {
+        require(initialized, "Contract has not been initialized yet! Wait the admin to start it");
         
         if(ref == msg.sender) {
             ref = address(0);
@@ -80,12 +92,14 @@ contract ApeSkulls {
         claimedSkulls[msg.sender] = 0;
         lastCollected[msg.sender] = block.timestamp;
         
-        claimedSkulls[referrals[msg.sender]] = claimedSkulls[referrals[msg.sender]] + skullsUsed / 8;
+        claimedSkulls[referrals[msg.sender]] = claimedSkulls[referrals[msg.sender]] + (skullsUsed / 10);
         marketSkulls = marketSkulls + (skullsUsed / 5);
     }
     
     function sellSkulls() public {
-        require(initialized);
+        require(initialized, "Contract has not been initialized yet! Wait the admin to start it");
+        require(getNextDepositTime(msg.sender) == 0, "You cannot sell your skulls yet. It's for sustenability sake and for the community health!");
+
         uint256 hasSkulls = getMySkulls(msg.sender);
         uint256 skullValue = calculateSkullSell(hasSkulls);
         uint256 fees = _devFee(skullValue);
@@ -116,6 +130,13 @@ contract ApeSkulls {
     
     function getBalance() public view returns(uint256) {
         return blackApe.balanceOf(address(this));
+    }
+
+    function getNextDepositTime(address adr) public view returns(uint256) {
+        if(block.timestamp - lastCollected[adr] >= 1 weeks) 
+            return 0;
+        
+        return 1 weeks - (block.timestamp - lastCollected[adr]);
     }
     
     function getMyMiners(address adr) public view returns(uint256) {
